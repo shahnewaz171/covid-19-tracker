f0baa1f1c8137d1a46f84210f48eb4c661c9ed09
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-10 18:23:49 +0600

    simple update

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ fetch('https://disease.sh/v3/covid-19/all')
 const showWorldInfo = (worldCases) => {
     const worldInfo = document.getElementById('world-info');
 
-    console.log(worldCases);
+    // console.log(worldCases);
     // world-cases
     document.getElementById('world-cases').innerText = worldCases.cases;
     // world-deaths
@@ -37,3 +37,43 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 // https://disease.sh/v3/covid-19/countries
+
+
+
+//ALl Countries
+const showAllCountry = () => {
+    fetch('https://disease.sh/v3/covid-19/countries')
+    .then(Response => Response.json())
+    .then(data => {
+        // console.log(data);
+        renderAllCountry(data);
+    });
+}
+
+const renderAllCountry = allCountry => {
+    const all =document.getElementById("table-body");
+    document.getElementById('world-info').style.display = 'none';
+    document.getElementById('allInfo').style.display = 'block';
+    document.getElementById('home').style.color = 'rgba(255,255,255,.5)';
+
+    allCountry.forEach(cases => {
+        console.log(cases);
+
+        const listDiv = document.createElement('tr');
+        const countryInfo = `
+            <td>${cases.country}</td>
+            <td>${cases.cases}</td>
+            <td>${cases.todayCases}</td>
+            <td>${cases.deaths}</td>
+            <td>${cases.todayDeaths}</td>
+            <td>${cases.recovered}</td>
+            <td>${cases.active}</td>
+            <td>${cases.critical}</td>
+            <td>${cases.tests}</td>
+            <td>${cases.population}</td>
+            <td>${cases.continent}</td>
+        `;
+        listDiv.innerHTML = countryInfo;
+        all.appendChild(listDiv);
+    });
+};
